6f6be90e6753fd6afd9dc4881cb8442587dff3c2
rui623 <72698680+rui323@users.noreply.github.com>
2024-09-10 17:49:11 +0900

    Add files via upload

--- detect.py
+++ detect.py
@@ -3,69 +3,94 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # クリックした点を格納するリスト
-points_list = []
+# points_list = []
+
+def resize_func(img):
+    width = 1280
+    h,w = img.shape[:2]
+
+    aspect_ratio= h / w
+    new_height = int(width * aspect_ratio)
+
+    # resized_img = cv2.resize(img, (width,new_height),interpolation=cv2.INTER_AREA)
+    resized_img = cv2.resize(img, (1280,720),interpolation=cv2.INTER_AREA)
+    return resized_img
 
 # 2点間の距離を計算する関数
 def dist(p1, p2):
     return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
 
 # マウスイベント処理関数
-def mouseEvents(event, x, y, flags, param):
+def mouseEvents(event, x, y, flags, points_list):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(x, y)
         points_list.append([x, y])
 
-# 画像を読み込み
-img = cv2.imread("./PNG_E/Pattern_1/1.5F_Pole/az30.png")  # 画像のパスを指定
+def adapt(img):
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    dst_cv = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,1001,20)
+    cv2.imshow("image",dst_cv)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-# 画像を表示してマウスイベントを設定
-cv2.imshow("Select the 4 points", img)
-cv2.setMouseCallback("Select the 4 points", mouseEvents)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # cv2.imwrite("image.png",dst_cv)
 
-# クリックした4つの点を取得
-points = np.array(points_list, dtype="float32")
-print("Selected points:", points)
+    black_area = np.sum(dst_cv == 0)
+    white = np.sum(dst_cv == 255)
+    size = black_area + white
+    # print(size)
+    # print(black_area, white )
+    area = black_area / size * 100
+    # print(area)
+    return area
 
-# 最も長い辺の長さを計算
-lengths = [dist(points[i], points[(i+1) % 4]) for i in range(4)]
-max_length = int(max(lengths))
+def extract_test_piece(img,points_list):
+    # 画像を表示してマウスイベントを設定
+    cv2.imshow("Select the 4 points", img)
+    cv2.setMouseCallback("Select the 4 points", mouseEvents,points_list)
+    # 十分なクリックが行われるまで待機
+    while len(points_list) < 4:
+        cv2.waitKey(1)  # 小さな待機時間で処理を継続する
 
-# 正方形のターゲット座標を設定
-square = np.array([[0, 0], [max_length, 0], 
-                   [max_length, max_length], [0, max_length]], dtype="float32")
+    cv2.destroyAllWindows()
 
-# 射影変換行列を計算
-M = cv2.getPerspectiveTransform(points, square)
+    # クリックした4つの点を取得
+    points = np.array(points_list, dtype="float32")
+    print("Selected points:", points)
 
-# 変換後の画像サイズ
-output_size = (max_length, max_length)
+    # 最も長い辺の長さを計算
+    lengths = [dist(points[i], points[(i+1) % 4]) for i in range(4)]
+    max_length = int(max(lengths))
 
-# 射影変換を適用
-warped = cv2.warpPerspective(img, M, output_size)
+    # 正方形のターゲット座標を設定
+    square = np.array([[0, 0], [max_length, 0], 
+                    [max_length, max_length], [0, max_length]], dtype="float32")
 
-# 結果を表示
-cv2.imshow("Warped Image", warped)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # 射影変換行列を計算
+    M = cv2.getPerspectiveTransform(points, square)
 
-cv2.imwrite("Detect_png.png",warped)
+    # 変換後の画像サイズ
+    output_size = (max_length, max_length)
 
-img1 = cv2.imread("Detect_png.png",cv2.IMREAD_GRAYSCALE)
-dst_cv = cv2.adaptiveThreshold(img1, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 1001, 50)
-                                                    # 軸目盛、軸ラベルを消す
-                                                  # 1行3列の3番目(右)の領域にプロットを設定
-cv2.imshow("image",dst_cv)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # 射影変換を適用
+    warped = cv2.warpPerspective(img, M, output_size)
+    return warped
 
-cv2.imwrite("image.png",dst_cv)
+def main():
+    # 画像を読み込み
+    # img = cv2.imread("../PNG_E/Pattern_1/1.5F_Pole/az-30.png")  # 画像のパスを指定
+    # img = resize_func(img)
+    # points_list = []
+    # new_img=extract_test_piece(img,points_list)
 
-black_area = np.sum(dst_cv == 0)
-white = np.sum(dst_cv == 255)
-size = black_area + white
-print(size)
-print(black_area, white )
-area = black_area / size * 100
-print(area)
+    # # 結果を表示
+    # cv2.imshow("Warped Image", new_img)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # cv2.imwrite("Detect_png.png",new_img)
+    new_img = cv2.imread("Detect_png.png")
+    print(adapt(new_img))
+
+if __name__ == '__main__':
+    main()
---
+++ image_subscriber.py
@@ -0,0 +1,53 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image  # 画像データを受信
+from std_msgs.msg import String  # 結果を返す
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+from pathlib import Path
+import sys
+sys.path.append(str(Path(__file__).resolve().parent))
+import detect
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(Image, 'image_topic', self.image_callback, 10)
+        self.result_publisher = self.create_publisher(String, 'result_topic', 10)
+        self.bridge = CvBridge()
+
+        self.points_list = []
+
+    def image_callback(self, msg):
+        try:
+
+            # 画像リセット
+            self.points_list = []
+            # ROS 画像メッセージを OpenCV 画像に変換
+            cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+            img = detect.resize_func(cv_image)
+            warped = detect.extract_test_piece(img,self.points_list)
+
+            h,w = warped.shape[:2]
+
+            area = detect.adapt(warped)
+            # 結果のサイズを計算し、テキストメッセージとしてパブリッシュ
+            result_msg = String()
+            result_msg.data = f'Width: {w}, Height: {h}, Area: {area}'
+            self.result_publisher.publish(result_msg)
+
+        except CvBridgeError as e:
+            self.get_logger().error(f'Failed to convert image: {e}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
